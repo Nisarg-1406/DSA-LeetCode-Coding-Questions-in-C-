0316e0516d9dae19e75b4bfc4c4c0861b42f9eeb
Nisarg Mehta <54138396+Nisarg-1406@users.noreply.github.com>
2020-09-28 23:57:13 +0530

    Create Subarray Product Less Than K.cpp

---
+++ Graphs/Subarray Product Less Than K.cpp
@@ -0,0 +1,19 @@
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        if(k == 0) return 0;
+        int start = 0;
+        int end = 0;
+        int prod = 1;
+        int count = 0;
+        while(end < nums.size()){
+            prod = prod * nums[end];
+            while(start < end && prod >= k){
+                prod = prod / nums[start];
+                start = start + 1;
+            }
+            if(prod < k) count = count + (end - start + 1);
+            end = end + 1;
+        }
+        return count;
+    }
