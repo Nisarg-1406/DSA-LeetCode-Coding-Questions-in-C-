a9f03d7febf6f62d0595ca64f2f6aea5b6cd96c7
Nisarg Mehta <54138396+Nisarg-1406@users.noreply.github.com>
2020-12-24 18:39:05 +0530

    Create Intersection of the Sorted linked list.cpp

---
+++ linked List/Intersection of the Sorted linked list.cpp
@@ -0,0 +1,11 @@
+    //Optimized solution - 
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA==NULL || headB == NULL) return NULL;
+        ListNode *temp1 = headA;
+        ListNode *temp2 = headB;
+        while(temp1 != temp2){
+            temp1 = temp1==NULL ? headB : temp1 -> next;
+            temp2 = temp2==NULL ? headA : temp2 -> next;
+        }
+        return temp1;
+    }
