7c3566ab64071dcc7202de8438406d3977654f5f
Nisarg Mehta <54138396+Nisarg-1406@users.noreply.github.com>
2020-12-26 19:06:30 +0530

    Update Minimum Cost For Tickets.cpp
    With Detailed Comments and 3 Approaches.

--- DP/Minimum Cost For Tickets.cpp
+++ DP/Minimum Cost For Tickets.cpp
@@ -14,21 +14,20 @@
           unordered_set<int> travel(begin(days), end(days));
           int dp[30] = {};
           for (int i = days.front(); i <= days.back(); ++i) {
-            if (travel.find(i) == travel.end()) dp[i % 30] = dp[(i - 1) % 30];
-            else dp[i % 30] = min({ dp[(i - 1) % 30] + costs[0],
-                dp[max(0, i - 7) % 30] + costs[1], dp[max(0, i - 30) % 30] + costs[2] });
+            if (travel.find(i) == travel.end()) dp[i % 30] = dp[(i - 1) % 30]; // considering that if not travelled on particular day then to count the previous day cost and doing %30
+            else dp[i % 30] = min({ dp[(i - 1) % 30] + costs[0], dp[max(0, i - 7) % 30] + costs[1], dp[max(0, i - 30) % 30] + costs[2] }); //Considering from last 7 days and 30 days and at last taking min from all 3 and doing %30.  
           }
           return dp[days.back() % 30];
         
-        //3. 
+        //3. Using Queue and considering the only days when we would be travelling 
         queue<pair<int, int>> last7, last30;
         int cost = 0;
         for (auto d : days) {
-            while (!last7.empty() && last7.front().first + 7 <= d) last7.pop();
-            while (!last30.empty() && last30.front().first + 30 <= d) last30.pop();
+            while (!last7.empty() && last7.front().first + 7 <= d) last7.pop(); //if day number is <= d then to pop from the queue
+            while (!last30.empty() && last30.front().first + 30 <= d) last30.pop(); //if day number is <= d then to pop from the queue
             last7.push({ d, cost + costs[1] });
             last30.push({ d, cost + costs[2] });
-            cost = min({ cost + costs[0], last7.front().second, last30.front().second });
+            cost = min({ cost + costs[0], last7.front().second, last30.front().second }); //min from the front of the queue. 
         }
         return cost;
     }
