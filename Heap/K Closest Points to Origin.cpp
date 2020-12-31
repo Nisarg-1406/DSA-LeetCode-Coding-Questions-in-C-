f7dc620ec93360701cc7ef0cd0e3c82456dd7397
Nisarg Mehta <54138396+Nisarg-1406@users.noreply.github.com>
2020-12-31 21:56:01 +0530

    Update K Closest Points to Origin.cpp
    5 Approaches - Great Solutions and Explanations:)

--- Heap/K Closest Points to Origin.cpp
+++ Heap/K Closest Points to Origin.cpp
@@ -1,3 +1,4 @@
+    Approach - 1 - Using Priority Queue and Pair
     * Using Max-heap as we want closest point  
     * Making pair of pair
     * Taking key as the pair first value which act as the distance from origin. 
@@ -20,3 +21,80 @@
         }
         return ans;
     }
+
+    Approach - 2 - Using Max Heap -> Using Priority Queue and Vector
+    CODE - 
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        priority_queue<vector<int>, vector<vector<int>>,compare> pq;
+        for (vector<int>& point : points) {
+            pq.push(point);
+            if (pq.size() > K) {
+                pq.pop();
+            }
+        }
+        vector<vector<int>> ans;
+        while (!pq.empty()) {
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+    struct compare {
+        bool operator()(vector<int>& p, vector<int>& q) {
+            return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
+        }
+    };
+
+
+    Approach - 3 - Using Min Heap - Using Priority Queue and Vector
+    CODE - 
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        priority_queue<vector<int>,vector<vector<int>>,compare> pq(points.begin(), points.end());
+        vector<vector<int>> ans;
+        for(int i =0;i<K;i++){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+    struct compare{
+        bool operator()(vector<int>& p,vector<int>& q){
+            return p[0]*p[0] + p[1]*p[1] > q[0]*q[0] + q[1]*q[1];
+        }
+    };
+
+    
+    Approach - 4 - Using MultiSet of Max Heap -
+    CODE -
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        multiset<vector<int>, compare> mset;
+        for(vector<int> &point : points){
+            mset.insert(point);
+            if (mset.size() > K) {
+                mset.erase(mset.begin()); //mp.end creates out the runtime error in this case
+            }
+        }
+        vector<vector<int>> ans;
+        copy_n(mset.begin(), K, back_inserter(ans));
+        return ans;
+    }
+    struct compare {
+        bool operator()(const vector<int>& p, const vector<int>& q) const {
+            return p[0] * p[0] + p[1] * p[1] > q[0] * q[0] + q[1] * q[1];
+        }
+    };
+
+    
+    Approach - 5 - Using MultiSet of Min Heap -
+    CODE -
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        multiset<vector<int>,compare> mset(points.begin(),points.end());
+        vector<vector<int>> ans;
+        copy_n(mset.begin(), K, back_inserter(ans));
+        return ans;
+    }
+    struct compare {
+        bool operator()(const vector<int>& p, const vector<int>& q) const {
+            return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
+        }
+    };
