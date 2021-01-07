3b6998c0741cc7daa07ed86bda6a46fdae04ceb4
Nisarg Mehta <54138396+Nisarg-1406@users.noreply.github.com>
2021-01-07 14:02:03 +0530

    Update Longest Substring Without Repeating Characters.cpp
    2 Approaches + Detailed explanation along with Dry run of second approach.

--- 2 pointer/Longest Substring Without Repeating Characters.cpp
+++ 2 pointer/Longest Substring Without Repeating Characters.cpp
@@ -26,3 +26,15 @@
         }
         return maxLen;
     }
+
+Dry Run for second approach - 
+    Here the example taken is -  "abcdbabcdefghi" - 
+   
+   "dict" is used to keep tracking the char in the input string you read every time. start indicates the position of starting position of the substring.
+    At the beginning, it initializes all value in "dict" to -1. Then in the for loop, it scans every char in the string. If the char in the "dict" value is larger than "start",
+   it means it already in the substring. You should change the start position of substring to the repeat position and start a new count. "maxLen" records the maximum length of
+   substring you have so far.
+    
+   For example, the input is "aba", you check dict[s[0]], which is dict[97] is -1. Therefore, you can change the dict[97] to 0. In this way, you keep recording the
+   string char's position in the dict. When you meet the second "a" in the input, dict[97] is 0 and larger than start, which is -1. Then you change start value to 0.
+   When you apply length function ( i - start), you calculate the new substring length, which didn't contain the substring before the first "a".
