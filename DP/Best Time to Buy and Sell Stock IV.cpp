aae6c8100c9c91c068752034176e4618d81bdf53
Nisarg Mehta <54138396+Nisarg-1406@users.noreply.github.com>
2021-01-17 15:00:07 +0530

    Create Best Time to Buy and Sell Stock IV.cpp

---
+++ DP/Best Time to Buy and Sell Stock IV.cpp
@@ -0,0 +1,19 @@
+    //T.C - O(N^2)
+    int maxProfit(int k, vector<int>& prices) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        int n = prices.size();
+        if (n == 0) return 0;
+        int dp[k+1][n];
+        memset(dp,0,sizeof(dp));
+        
+        for (int t = 1; t <= k; t++) {
+            int max = INT_MIN;
+            for (int d = 1; d < n; d++) {
+                if (dp[t-1][d-1] - prices[d-1] > max) max = dp[t-1][d-1] - prices[d-1];
+                if (max + prices[d] > dp[t][d-1]) dp[t][d] = max + prices[d];
+                else dp[t][d] = dp[t][d-1];
+            }
+        }
+        return dp[k][n-1];
+    }
