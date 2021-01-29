631e9b3b78dad487a0a8a20de8d725ee83edd425
Nisarg Mehta <54138396+Nisarg-1406@users.noreply.github.com>
2021-01-29 18:38:38 +0530

    2 approaches - Vertical Order Traversal of a Binary Tree

--- Trees/Vertical Order Traversal of a Binary Tree.cpp
+++ Trees/Vertical Order Traversal of a Binary Tree.cpp
@@ -1,3 +1,5 @@
+//     2 approaches - 
+
     map<int,map<int,set<int>>> mymap; 
     void solve(TreeNode *curr,int col,int row){
         if(!curr) return;
@@ -15,5 +17,32 @@
                     ans.back().push_back(m3);
             }
         }
+        return ans;
+    }
+
+//     Using BFS
+     vector<vector<int>> verticalTraversal(TreeNode* root) {
+        TreeNode* curr;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root,0});
+        vector<vector<int>> ans;
+        map<int,vector<int>> mymap;
+        while(!q.empty()){
+            int size = q.size();
+            map<int,set<int>> mapset;
+            while(size--) {
+                curr = q.front().first;
+                int col = q.front().second;
+                q.pop();
+                mapset[col].insert(curr->val);
+                if(curr->left) q.push({curr->left,col-1});
+                if(curr->right) q.push({curr->right,col+1});
+            }
+            for(auto it: mapset)
+                for(auto it2: it.second)
+                    mymap[it.first].push_back(it2);
+        }
+        for(auto it: mymap)
+            ans.push_back(it.second);
         return ans;
     }
