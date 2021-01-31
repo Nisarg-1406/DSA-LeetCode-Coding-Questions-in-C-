9ece62179b3f213e6c5fe0dde92585092396c00f
Nisarg Mehta <54138396+Nisarg-1406@users.noreply.github.com>
2021-01-31 14:50:05 +0530

    Create Next Permutation.cpp

---
+++ Arrays/Next Permutation.cpp
@@ -0,0 +1,18 @@
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int k,l;
+        for(k = n-2;k>=0;k--){
+            if(nums[k] < nums[k+1]) break;
+        }
+        if (k < 0) reverse(nums.begin(), nums.end());
+        else {
+            for(l = n-1;l>k;l--){
+                if (nums[l] > nums[k]) break;
+            }
+            swap(nums[k], nums[l]);
+    	    reverse(nums.begin() + k + 1, nums.end());
+        }
+        
+        //There is also the library function - 
+        next_permutation(begin(nums), end(nums));
+    }
