082bfcfb4ac8636981f7077d23af8c129d23787f
Nisarg Mehta <54138396+Nisarg-1406@users.noreply.github.com>
2021-02-01 14:08:44 +0530

    2 Approaches - Number of 1 Bits

--- Bit Manipulation/Number of 1 Bits.cpp
+++ Bit Manipulation/Number of 1 Bits.cpp
@@ -6,4 +6,13 @@
             n >>= 1; 
         }
         return count;
+        
+        - Using right most set bits (rsbm)
+        int cnt = 0;
+        while (n != 0) {
+            int rsbm = n & -n;
+            n = n - rsbm;
+            cnt = cnt + 1;        
+        }
+        return cnt;
     }
