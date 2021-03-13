202ffad588f6075270a894728067886017539ae4
Nisarg Mehta <54138396+Nisarg-1406@users.noreply.github.com>
2021-03-13 18:03:23 +0530

    2 Methods - Coin change

--- DP/Coin Change.cpp
+++ DP/Coin Change.cpp
@@ -1,31 +1,6 @@
 
-    // 3 Methods - 
-    // Method - 1
-    int coinChange(vector<int>& coins, int amount) {
-        int len = coins.size();
-        int t[len + 1][amount + 1]; 
-        for (int j = 0; j < amount+1; j++) {
-            t[0][j] = INT_MAX - 1;
-        }
-        for (int i = 1; i < len+1; i++) {
-            t[i][0] = 0;
-        }
-        int j;
-        for(int i=1;j<=amount;j++){
-            if(j%coins[0]==0) t[i][j] = coins[0];
-            else t[i][j] = INT_MAX - 1;
-        }
-        
-        for(int i = 1;i<len+1;i++){
-            for(int j = 1;j<amount+1;j++){
-                if(coins[i-1] <= j) t[i][j] = min(t[i-1][j] , t[i-1][j-coins[i-1]] + 1);
-                else t[i][j] = t[i-1][j];
-            }
-        }
-        return t[len][amount] < amount ? -1 : t[len][amount];
-    }
-    
-    // Method 2 - 
+    // 2 Methods - 
+    // Method 1 - 
     int coinChange(vector<int>& coins, int amount) {
 		if(amount==0) return 0;
 		int dp[coins.size()+1][amount+1];
@@ -61,7 +36,7 @@
         return dp[coins.size()][amount];
     }
     
-    // Method - 3 - 
+    // Method - 2 - 
     int coinChange(vector<int>& coins, int amount) {
         int Max = amount + 1;
         vector<int> dp(amount + 1, Max);
