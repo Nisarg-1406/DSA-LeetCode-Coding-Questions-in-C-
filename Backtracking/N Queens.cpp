73a578e9cc37fe0fe221f5b6ae6ed079aedcd4e1
Nisarg Mehta <54138396+Nisarg-1406@users.noreply.github.com>
2021-05-22 16:56:23 +0530

    Create N Queens.cpp

---
+++ Backtracking/N Queens.cpp
@@ -0,0 +1,35 @@
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
+        vector<string> nQueens(n,string(n, '.'));
+        solveNQueens(res, nQueens, 0, n);
+        return res;
+    }
+    void solveNQueens(vector<vector<std::string>> &res, vector<string> &nQueens, int row, int n) {
+        if (row == n) {
+            //cout<<"The row is : "<<row<<"\n";
+            res.push_back(nQueens);
+            return;
+        }
+        for (int col = 0; col != n; ++col)
+            if (isValid(nQueens, row, col, n)) {
+                nQueens[row][col] = 'Q';
+                //cout<<row<<" "<<col<<"\n";
+                solveNQueens(res, nQueens, row + 1, n);
+                nQueens[row][col] = '.';
+            }
+    }
+    bool isValid(vector<string> &nQueens, int row, int col, int n) {
+        //check if the column had a queen before.
+        for (int i = 0; i != row; ++i)
+            if (nQueens[i][col] == 'Q')
+                return false;
+        //check if the 45° diagonal had a queen before.
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j)
+            if (nQueens[i][j] == 'Q')
+                return false;
+        //check if the 135° diagonal had a queen before.
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j)
+            if (nQueens[i][j] == 'Q')
+                return false;
+        return true;
+    }
